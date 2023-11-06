50e2c8676bc9af6211e8f7ac94545cef986215be
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-11-06 14:46:59 -0300

    feat: adicionando numeros aleatorios

--- mpi.py
+++ mpi.py
@@ -1,16 +1,29 @@
 from mpi4py import MPI
 import numpy as np
+import pandas as pd
+
+ROWS = 4
+COLS = 10
+
+def gen_matrix(rows, cols):
+    matrix = []
+
+    for _ in range(rows):
+        matrix.append([np.random.random() for _ in range(cols)])
+    return matrix
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
 # Duas matrizes de exemplo
-matrix_A = np.array([[1.0, 2.0], [2.0, 4.0]])
-matrix_B = np.array([[1.0, 2.0], [2.0, 4.0]])
+matrix_A = np.array(gen_matrix(ROWS, COLS))
+matrix_B = np.array(gen_matrix(ROWS, COLS))
+
 # Tamanho das matrizes
-rows_A, cols_A = matrix_A.shape
-rows_B, cols_B = matrix_B.shape
+rows_A, cols_A = len(matrix_A), len(matrix_A[0])
+rows_B, cols_B = len(matrix_B), len(matrix_B[0])
+
 
 # Tamanho das matrizes resultantes
 result_rows = rows_A * rows_B
@@ -45,4 +58,4 @@ comm.Gather(local_result, result, root=0)
 # O processo 0 imprime a matriz resultante
 if rank == 0:
     print("Resultado do Produto de Kronecker:")
-    print(result)
+    print(result.round(4))
